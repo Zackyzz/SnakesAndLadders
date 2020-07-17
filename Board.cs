ee12e8c47136620f2ab3c78af5c503759c301473
Zaharia Burghelea <legendaryproyi@yahoo.com>
2020-07-17 17:36:50 +0300

    Created a square.

---
+++ Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeuriSiLaderuri
+{
+    class Board
+    {
+        public void Show(Form form)
+        {
+            Square square = new Square
+            {
+                height = 100,
+                width = 100,
+                x = 0,
+                y = 0,
+                color = Color.Blue,
+                number = 7
+            };
+            square.createSquare(form);
+        }
+
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeuriSiLaderuri
+{
+    public class Square
+    {
+        public int number = 7;
+        public int x, y;
+        public int height, width;
+        public Color color;
+
+        public void createSquare(Form f)
+        {
+            Panel panel = new Panel
+            {
+                BackColor = color,
+                Width = width,
+                Height = height,
+                Top = y,
+                Left = x
+            };
+            Label label = new Label();
+            label.Dock = DockStyle.Fill;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Text = number.ToString();
+            panel.Controls.Add(label);
+
+            f.Controls.Add(panel);
+        }
+    }
+}
